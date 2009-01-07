505a0e24da7b5774c4e4532c45f0e972eda91796
Steve Spencer <steve@bigfrog.net>
2009-01-06 17:45:52 -0800

    Stylized HTB with 2 examples ... 1 col and 2 cols.

---
+++ php/site.php
@@ -0,0 +1,99 @@
+<?php
+define("NAV_HOME", "home");
+define("NAV_IRC", "irc");
+define("NAV_SERVICE", "service");
+define("NAV_BLOG", "blog");
+define("NAV_ABOUT", "about");
+define("COLS_1", "col_1");
+define("COLS_2", "col_2");
+
+$SITE_MENU = array(
+    NAV_HOME    => array("/", "Home"),
+    NAV_IRC     => array("/irc/", "IRC"),
+    NAV_SERVICE => array("/services/", "Services"),
+    NAV_BLOG    => array("/blog/", "Blog"),
+    NAV_ABOUT   => array("/about/", "About")
+);
+
+function site_header($selected_tab, $page_title, $cols)
+{
+    global $SITE_MENU, $__NumCols__;
+
+    $__NumCols__ = $cols;
+    $cols_clz = ($cols == COLS_2 ? "class=\"yui-t4\"" : "");
+    $form = ""; // or the below if we want search
+    /* 
+        <form class="clearfix" action="http://search.yahoo.com/search" method="get">
+            <input name="vs" type="hidden" value="developer.yahoo.com">
+            <input name="vs" type="hidden" value="yuiblog.com">
+            <input name="vs" type="hidden" value="yuilibrary.com">
+            <input type="search" id="sitesearchbox" name="p">
+            <input type="button" name="some_name" value="Search" id="sitesearchbutton">
+        </form>
+    */
+    
+    $nav_menu = "";
+    foreach($SITE_MENU as $tab => $arr) {
+        $url = $arr[0];
+        $label = $arr[1];
+        $clz = ($tab == $selected_tab ? "class=\"on\"" : "");
+        $nav_menu .= "<li $clz><a href=\"$url\">$label</a></li>";
+    }
+    
+
+echo <<< EOS
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <title>$page_title</title>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.6.0/build/reset-fonts-grids/reset-fonts-grids.css">
+    <link rel="stylesheet" type="text/css" href="/css/site.css">
+</head>
+<body>
+    <div id="doc3" $cols_clz>
+        <a name="top"></a>
+        <div id="hd" class="clearfix">
+            $form
+            <div class="logos">
+                <img id="logo" src="/img/logo.png" width="291" height="23" alt="Hack the Browser">
+                <img id="tagline" src="/img/tagline.png" width="416" height="13" alt="Playground for the development of open-source, scriptable browser plugins"> 
+            </div>
+        </div>
+
+        <div id="nav" class="navtabs clearfix">
+            <ul>
+                $nav_menu
+            </ul>
+       </div>
+
+        <div id="bd">
+            <div id="yui-main">
+                <div class="yui-b">
+EOS;
+}
+
+function site_sidebar()
+{
+    echo "</div></div>";
+    echo "<div class=\"yui-b secondary\">";
+}
+
+function site_footer()
+{
+    global $__NumCols__;
+
+    $ending_divs = ($__NumCols__ == COLS_2) ? "</div>" : "</div></div>";
+
+    echo <<< EOS
+            $ending_divs
+        </div>
+        <div id="ft">FOOTER IS HERE</div>
+    </div>
+</body>
+</html>
+EOS;
+}
+?>
---
+++ www/col1.php
@@ -0,0 +1,14 @@
+<?php
+require "/usr/local/www/hackthebrowser.org/php/site.php";
+site_header(NAV_HOME, "Hack the Browser", COLS_1);
+?>
+<h1>Welcome to Hack the Browser</h1>
+<p>Test here.</p>
+<h2>Site Header 2</h2>
+<p>Lots of text -- not really.</p>
+<p>Lots of text -- not really.</p>
+
+
+<?php
+site_footer();
+?>
