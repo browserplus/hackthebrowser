6771bd6bf417065e36b1cc11b850e422c6a0845d
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-03-12 17:10:01 -0600

    revise homepage langauge, spelling and mention IRC & collaboration.

--- www/index.php
+++ www/index.php
@@ -11,7 +11,7 @@ site_header(NAV_HOME, "Hack the Browser", COLS_2);
     are brief descriptions of all known open source projects and hacks
 	for the BrowserPlus platform.  These hacks will be either in ruby or
 	native code and are built to put new functionality within the reach
-	of browser based javascript.
+	of browser based JavaScript.
 </p>
 <p>
     You can find a ton more information about BrowserPlus over at
@@ -21,7 +21,7 @@ site_header(NAV_HOME, "Hack the Browser", COLS_2);
 	popular environments.
 </p>
 
-<h2> Our Projects </h2>
+<h2> The Projects Here </h2>
 
 <p>
     Most of the projects you'll find here are hosted on
@@ -29,17 +29,29 @@ site_header(NAV_HOME, "Hack the Browser", COLS_2);
 	on github certainly isn't a requirement, but if you haven't played
 	with it, give it a look.  Simply because git and github were
 	designed from the ground up with geographically distributed
-	colloaboration in mind, it'll ultimately be less work to get services
+	collaboration in mind, it'll ultimately be less work to get services
     onto the official BrowserPlus distribution servers.
+</p>
+
+<h2>Collaborate</h2>
+<p>
+    Come join us on irc on <tt>irc.freenode.net #browserplus</tt>.
+	IRC is archived and searchable on this site, just hit that "IRC" tab
+	above. Or hop on over to the
+	<a href="http://developer.yahoo.net/forum/index.php?showforum=90">official discussion forums</a>.
 </p>
 
 <h2>Contribute</h2>
 
 <p>
-    Want to hack on an existing project?  fork it!  Have a new idea or
-	implementation that you want to get listed?  Just drop us an email
-	&lt;browserplus-feedback 4t yahoo-inc d0t com&gt;, and we'll get you
-	listed.
+    <b>Want to hack on an existing project?</b>
+	<br>
+	fork it on github!
+</p>
+<p>
+    <b>Have a new idea or implementation that you want to get listed?</b><br>
+	Just drop us an email &lt;browserplus-feedback 4t yahoo-inc d0t com&gt;,
+	and we'll get you added.
 </p>
 
 <h2>About HackTheBrowser.org</h2>
