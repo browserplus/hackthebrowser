0b402a7fc78b239f9dcf76c916498294c12e60ff
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-03-17 12:37:36 -0600

    add gitignore and add newline to fetch_commit.sh

--- bin/fetch_commit.sh
+++ bin/fetch_commit.sh
@@ -4,4 +4,4 @@ for x in /usr/local/www/hackthebrowser.org/projects/*/meta.json; do
     dir=`dirname $x`
     dir=`basename $dir`
     curl -s http://hackthebrowser.org/fetch/$dir/commit > /dev/null
-done    
+done
