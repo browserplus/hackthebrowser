67d2deff8464c962c20be6f787b558130ca34807
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-03-17 12:39:26 -0600

    fix fetch_commit.sh, steve made mods directly in the checked out copy

--- bin/fetch_commit.sh
+++ bin/fetch_commit.sh
@@ -3,5 +3,6 @@
 for x in /usr/local/www/hackthebrowser.org/projects/*/meta.json; do
     dir=`dirname $x`
     dir=`basename $dir`
-    curl -s http://hackthebrowser.org/fetch/$dir/commit > /dev/null
+    echo "******* $dir *******"
+    curl -s http://hackthebrowser.org/fetch/$dir/commit
 done
